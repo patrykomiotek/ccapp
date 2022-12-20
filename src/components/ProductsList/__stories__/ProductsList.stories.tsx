3f3bffef29b0e0e87c7922375fad539c6a8746dd
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-12-20 15:50:50 +0100

    feat: tests for producs list

---
+++ src/components/ProductsList/__stories__/ProductsList.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta } from '@storybook/react';
+
+import { ProductsList } from './ProductsList';
+
+export default {
+  title: 'App/Products/ProductsList',
+} as ComponentMeta<typeof ProductsList>;
+
+
+export const _ProductsList = () => (
+  <ProductsList />
+);
